c03a1f5c66ecac70792656ada464a97ba0224d2b
M074MED <M074MED@users.noreply.github.com>
2025-01-14 23:30:56 +0000

    deploy: 7480e853a5d4a6b70065b1f5e4af75130e4c4412

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2577910322"
+    serviceWorkerVersion: "2117761845"
   }
 });
