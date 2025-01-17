e2c86d8f435f41d3226c495c368feadc9c2b78af
M074MED <mhmedmhmed1234951@gmail.com>
2025-01-17 14:35:48 +0200

    Refactor GitHub Actions workflows to install dependencies earlier and print .env file contents

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -18,6 +18,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
+      print("Key: ${dotenv.env.toString()}");
       return web;
     }
     switch (defaultTargetPlatform) {
